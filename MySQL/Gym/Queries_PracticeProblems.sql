7d7ecf3143c18b8f29ea8b105aa691678c1821fc
Sarvesh Prajapati <47186476+Sarvesh-Prajapati@users.noreply.github.com>
2025-05-19 09:46:48 +0530

    Update Queries_PracticeProblems.sql

--- MySQL/Gym/Queries_PracticeProblems.sql
+++ MySQL/Gym/Queries_PracticeProblems.sql
@@ -292,6 +292,31 @@ WITH RECURSIVE explode AS
 ) SELECT id, REGEXP_REPLACE(colors, ',.*', '') colors FROM explode ORDER BY id;
 
 
+-- #######################################################################################################################################
+-- ----------------------- COMPLEX QUERIES (Ankit Bansal YT Channel : https://www.youtube.com/watch?v=rFsgzSeG1X0) -----------------------
+-- Tables used : ab_employees, ab_salary_history
+
+-- --------- Create and populate ab_employees table
+CREATE TABLE ab_employees (employee_id INT PRIMARY KEY, emp_name VARCHAR(10) NOT NULL, join_date DATE NOT NULL, department VARCHAR(10) NOT NULL);
+INSERT INTO ab_employees(employee_id, emp_name, join_date, department)
+VALUES (1, 'Alice', '2018-06-15', 'IT'),(2, 'Bob', '2019-02-10', 'Finance'),(3, 'Charlie', '2017-09-20', 'HR'),
+(4, 'David', '2020-01-05', 'IT'),(5, 'Eve', '2016-07-30', 'Finance'),(6, 'Sumit', '2016-06-30', 'Finance');
+
+-- --------- Create and populate salary_history table
+CREATE TABLE ab_salary_history (employee_id INT,change_date DATE NOT NULL,salary DECIMAL(10,2) NOT NULL,promotion VARCHAR(3));
+INSERT INTO ab_salary_history (employee_id, change_date, salary, promotion)
+VALUES(1, '2018-06-15', 50000, 'No'),(1, '2019-08-20', 55000, 'No'),(1, '2021-02-10', 70000, 'Yes'),(2, '2019-02-10', 48000, 'No'),
+    (2, '2020-05-15', 52000, 'Yes'),(2, '2023-01-25', 68000, 'Yes'),(3, '2017-09-20', 60000, 'No'),(3, '2019-12-10', 65000, 'No'),
+    (3, '2022-06-30', 72000, 'Yes'),(4, '2020-01-05', 45000, 'No'),(4, '2021-07-18', 49000, 'No'),(5, '2016-07-30', 55000, 'No'),
+    (5, '2018-11-22', 62000, 'Yes'),(5, '2021-09-10', 75000, 'Yes'),(6, '2016-06-30', 55000, 'No'),(6, '2017-11-22', 50000, 'No'),
+    (6, '2018-11-22', 40000, 'No'),(6, '2021-09-10', 75000, 'Yes');
+
+
+
+
+
+
+
 
 -- SELECT LENGTH("APPLE") - LENGTH(REGEXP_REPLACE("APPLE", '[aeiouAEIOU]', ''));  -- Counting the no. of vowels in a word (2)
 -- SELECT LENGTH("APPLE") - LENGTH(REGEXP_REPLACE("APPLE", '[^aeiouAEIOU]', ''));  -- Counting the consonants in a word  (3)
