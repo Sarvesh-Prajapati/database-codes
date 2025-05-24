f2e6e678a633defb39a5e17d4b87ad0c20fe7a17
Sarvesh Prajapati <47186476+Sarvesh-Prajapati@users.noreply.github.com>
2025-05-24 22:30:56 +0530

    Update Queries_db_sales.sql

--- MySQL/DB_Sales/Queries_db_sales.sql
+++ MySQL/DB_Sales/Queries_db_sales.sql
@@ -21,7 +21,7 @@ DESCRIBE sales;
 -- customer, create a new table 'new_sales' as follows:
 CREATE TABLE new_sales AS
 SELECT
-	L.cust_id
+    L.cust_id
     , S.cust_name
     , S.sp_assigned
     , S.category
@@ -36,7 +36,7 @@ ALTER TABLE new_sales RENAME sales;  -- Rename the 'new_sales' table to 'sales' 
 
 -- 1. Calculate the sales by state in each region and leads' conversion count by state in each region.
 SELECT
-	L.region
+    L.region
     , L.state
     , SUM(S.sales) AS state_sales
     , COUNT(S.cust_id) AS state_conversion_count
