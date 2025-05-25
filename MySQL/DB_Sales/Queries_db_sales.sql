9a69f5097b2ce504290ce27f0fd4c2a1d13600ee
Sarvesh Prajapati <47186476+Sarvesh-Prajapati@users.noreply.github.com>
2025-05-25 11:55:29 +0530

    Update Queries_db_sales.sql

--- MySQL/DB_Sales/Queries_db_sales.sql
+++ MySQL/DB_Sales/Queries_db_sales.sql
@@ -4,7 +4,9 @@ SELECT * FROM db_sales.leads;
 SELECT * FROM db_sales.sales;
 SELECT * FROM db_sales.targets;
 
--- ALTER TABLE leads DROP postal_code;
+ALTER TABLE targets MODIFY COLUMN target DECIMAL(10, 2);  -- changing 'target' column's dtype to DECIMAL type
+ALTER TABLE targets RENAME COLUMN target TO target_amt;   -- renaming col name to be different from table name
+ALTER TABLE leads DROP postal_code;  -- dropping unneeded column
 
 SELECT *
 FROM INFORMATION_SCHEMA.TABLES 
@@ -15,18 +17,18 @@ AND TABLE_NAME = 'sales';
 DESCRIBE leads;
 DESCRIBE sales;
 
--- ALTER TABLE sales DROP cust_id;  -- dropped 'cust_id' col as it was not an ID, rather just a SL. No.
+ALTER TABLE sales DROP cust_id;  -- dropped 'cust_id' col as it was not an ID, rather just a SL. No.
 
 -- Since 'sales' table doesn't have 'cust_id' col in it now, and we need an ID for each
 -- customer, create a new table 'new_sales' as follows:
 CREATE TABLE new_sales AS
 SELECT
-    L.cust_id
+	L.cust_id
     , S.cust_name
     , S.sp_assigned
     , S.category
     , S.order_date
-    , S.sales 
+    , S.sales_amt 
 FROM leads L JOIN sales S ON L.cust_name = S.cust_name; -- joining on 'cust_name' as all 'cust_name' vals in tbl 'leads' are unique
 
 DROP TABLE sales;  -- Drop the current 'sales' table
@@ -36,13 +38,14 @@ ALTER TABLE new_sales RENAME sales;  -- Rename the 'new_sales' table to 'sales' 
 
 -- 1. Calculate the sales by state in each region and leads' conversion count by state in each region.
 SELECT
-    L.region
+	L.region
     , L.state
-    , SUM(S.sales) AS state_sales
+    , SUM(S.sales_amt) AS state_sales
     , COUNT(S.cust_id) AS state_conversion_count
 FROM leads L JOIN sales S ON L.cust_id = S.cust_id
 GROUP BY region, state
 ORDER BY region, state;
+
 
 -- 2. 
 
