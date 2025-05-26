383bec80fc275c46b9d353380499734c0dd30249
Sarvesh Prajapati <47186476+Sarvesh-Prajapati@users.noreply.github.com>
2025-05-26 22:53:59 +0530

    Update Create_db_sales.sql

--- MySQL/DB_Sales/Create_db_sales.sql
+++ MySQL/DB_Sales/Create_db_sales.sql
@@ -36,6 +36,20 @@ sales_person VARCHAR(30),
 target DECIMAL(10, 2)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+-- CSV files were imported into above 3 tables using 'Table Data Import Wizard' utility.
+-- We can also load the CSVs into the above tables using LOAD DATA statement.
+-- First copy-paste CSVs in locatino 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\' , otherwise --secure-file-priv error is raised
+-- Then edit and run the following statement, thrice for loading 3 CSVs in the 3 tables created above:
+ 
+LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\CSV_FILE_NAME_HERE.csv'  -- path where CSVs MUST be present
+INTO TABLE table_into_which_CSV_is_to_load_here
+FIELDS TERMINATED BY ','
+-- ENCLOSED BY ''         -- uncomment if applicable
+-- ESCAPED BY '\\'        -- uncomment if applicable
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;           -- if topmost record in CSV is header, this line skips it
+
+
 
 
 
