45e9a8e44fa98136cd74ea86b389537543c43aa8
Sarvesh Prajapati <47186476+Sarvesh-Prajapati@users.noreply.github.com>
2025-05-26 15:49:43 +0530

    Update Queries_db_sales.sql

--- MySQL/DB_Sales/Queries_db_sales.sql
+++ MySQL/DB_Sales/Queries_db_sales.sql
@@ -6,7 +6,7 @@ SELECT * FROM db_sales.targets;
 
 ALTER TABLE targets MODIFY COLUMN target DECIMAL(10, 2);  -- changing 'target' column's dtype to DECIMAL type
 ALTER TABLE targets RENAME COLUMN target TO target_amt;   -- renaming col name to be different from table name
-ALTER TABLE leads DROP postal_code;  -- dropping unneeded column
+ALTER TABLE leads DROP postal_code;  -- dropping redundant column
 
 SELECT *
 FROM INFORMATION_SCHEMA.TABLES 
