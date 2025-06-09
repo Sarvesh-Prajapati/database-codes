38b91dfb85d5e516553d66552dbb61656c08fc7a
Sarvesh Prajapati <47186476+Sarvesh-Prajapati@users.noreply.github.com>
2025-06-09 21:11:50 +0530

    Update Create_AccidentData_Spatial.sql

--- MySQL/Gym/Create_AccidentData_Spatial.sql
+++ MySQL/Gym/Create_AccidentData_Spatial.sql
@@ -1,4 +1,4 @@
--- When loading a huge file ('AccidentData.csv' here), it's better to first load a small subset of it.
+-- When loading a huge file ('AccidentData.csv' here), it's better to first load a small subset of it (e.g. 'AccidentDataTEST.csv' present in the LHS in the tree hierarchy)
 -- So, 'AccidentDataTEST.csv' with 30 rows was created, opened, & its 2 columns 'Accident_Date' & 'Time' were combined into a single column 'Accident_Timestamp'. Saved as CSV 'AccidentDataTEST_GEO.csv'.
 -- File 'AccidentDataTEST_GEO.csv' was then opened, & its 2 columns 'Latitude' & 'Longitude' were combined as col 'LatLong' as VARCHAR (basically, string type) because direct import of spatial info
 -- is not possible in MySQL. Then, 'Latitude' & 'Longitude' cols were deleted from CSV. Next, after saving changes, all 30 rows of 'AccidentDataTEST_GEO.csv' were loaded into 
@@ -17,8 +17,8 @@ Accident_Timestamp TIMESTAMP,
 Day_Of_Week VARCHAR(10),
 Junction_Control VARCHAR(100),
 Junction_Detail VARCHAR(100),
-Accident_Severity VARCHAR(20),             -- slight, serious, fatal
-LatLong VARCHAR(50),                       -- CSV file has 'LatLong' vals as string (e.g. 'POINT(10.123 -10.123)') so VARCHAR specified here; fixed below after loading data 
+Accident_Severity VARCHAR(20),            -- slight, serious, fatal
+LatLong VARCHAR(50),                      -- CSV file has 'LatLong' vals as string (e.g. 'POINT(10.123 -10.123)') so VARCHAR specified here; fixed below after loading data 
 Light_Conditions VARCHAR(50),
 Local_Authority_District VARCHAR(50),
 Carriageway_Hazards VARCHAR(100),
@@ -37,7 +37,7 @@ CREATE INDEX idx_accident_timestamp ON accident_data (Accident_Timestamp);
 
 SELECT * FROM accident_data;
 
--- TRUNCATE sql_forda.accident_data;
+TRUNCATE sql_forda.accident_data;
 
 -- -------- LOAD THE ACCIDENT DATA FROM THE CSV:
 LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\AccidentData.csv' 
@@ -45,22 +45,26 @@ INTO TABLE accident_data
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 LINES;
 
-SELECT * FROM accident_data;
+-- SELECT * FROM accident_data;
+SELECT Lat_Long FROM accident_data;
 
 ALTER TABLE accident_data ADD COLUMN Lat_Long POINT;   -- adding new col, of POINT data type, to store spatial values
 UPDATE accident_data SET Lat_Long = ST_GeomFromText(REPLACE(LatLong, "'", ''));    -- moving str vals of 'LatLong' (of VARCHAR type e.g. 'POINT(10.123 -10.123)') to 'Lat_Long' (of POINT type) 
 
 ALTER TABLE accident_data MODIFY COLUMN Lat_Long POINT AFTER LatLong;  -- moving the 'Lat_Long' col from the last pos to right beside 'LatLong' col
 
-
 SELECT * FROM accident_data;    -- shows 'Lat_long' vals as BLOB objects as it should
 
 -- We can now drop the VARCHAR type 'LatLong' col as it is no longer needed but let's just keep it since 'Lat_Long' vals are displayed as BLOB
 -- ALTER TABLE accident_data DROP COLUMN LatLong;
 
--- One can extract the Lat and Long vals from BLOB geometry value as follows:
+-- ##########################################################################################################################################
+-- ------ One can extract the Lat and Long vals from BLOB geometry value in following ways:
+
+SELECT ST_AsText(Lat_Long) AS ReadablePoint FROM accident_data;   -- Displays the BLOB spatial info as readable text
+
 SELECT
-    accident_id AS Acc_ID
+	accident_id AS Acc_ID
     , ST_X(Lat_Long) AS Latitude        -- ST_X for x-coordinate or Latitude
     , ST_Y(Lat_Long) AS Longitude       -- ST_Y for y-coordinate or Longitude
 FROM accident_data WHERE accident_id = 1;
