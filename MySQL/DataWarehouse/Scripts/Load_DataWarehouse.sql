da3b8899c7966b63ee898210ffe622b3ff3c99a0
Sarvesh Prajapati <47186476+Sarvesh-Prajapati@users.noreply.github.com>
2025-06-18 00:57:09 +0530

    Update Load_DataWarehouse.sql

--- MySQL/DataWarehouse/Scripts/Load_DataWarehouse.sql
+++ MySQL/DataWarehouse/Scripts/Load_DataWarehouse.sql
@@ -2,14 +2,17 @@
 
 SHOW VARIABLES LIKE 'secure_file_priv';  -- returns path where the files to be loaded must be present
 
--- Paste the three raw files into the path returned by the above query and then run the following LOAD DATA command: 
+-- Paste the three raw files into the path returned by the above query and then run the following :
+
+TRUNCATE TABLE bronze_crm_cust_info;
 
 LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\cust_info.csv'
+IGNORE                             -- IGNORE deprecates errors to warnings and file continues loading
 INTO TABLE bronze_crm_cust_info
 CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
+IGNORE 1 LINES ;
 
 
 
