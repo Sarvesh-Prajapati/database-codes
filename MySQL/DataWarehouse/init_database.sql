4760534e504adaf18f3915eabd3cdb65b4396b33
Sarvesh Prajapati <47186476+Sarvesh-Prajapati@users.noreply.github.com>
2025-06-17 22:09:11 +0530

    Delete MySQL/DataWarehouse/init_database.sql

--- MySQL/DataWarehouse/init_database.sql
+++
@@ -1,6 +0,0 @@
--- This project is a replication of Bara Khatib Salkini's video: https://www.youtube.com/watch?v=9GVqKuTVANE
--- He created it in SSMS.
-
-DROP DATABASE IF EXISTS DataWarehouse;
-CREATE DATABASE DataWarehouse;
-USE DataWarehouse;
