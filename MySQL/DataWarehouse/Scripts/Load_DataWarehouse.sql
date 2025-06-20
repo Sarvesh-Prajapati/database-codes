331bac1c5e408ae8c99ec81f33d5e1fc117e6724
Sarvesh Prajapati <47186476+Sarvesh-Prajapati@users.noreply.github.com>
2025-06-20 23:24:30 +0530

    Update Load_DataWarehouse.sql

--- MySQL/DataWarehouse/Scripts/Load_DataWarehouse.sql
+++ MySQL/DataWarehouse/Scripts/Load_DataWarehouse.sql
@@ -158,5 +158,16 @@ SELECT
       END AS gen
 FROM datawarehouse.bronze_erp_cust_az12 ;
 
-
+-- -------------------- Loading 'silver_erp_loc_a101'
+INSERT INTO silver_erp_loc_a101(cid, cntry)
+SELECT
+	REPLACE(cid, '-', '') AS cid
+    , CASE
+		WHEN TRIM(REPLACE(REPLACE(cntry, CHAR(32), ''), CHAR(13), '')) = 'DE' THEN 'Germany' 
+		WHEN TRIM(REPLACE(REPLACE(cntry, CHAR(32), ''), CHAR(13), '')) IN ('US', 'USA', 'UnitedStates') THEN 'United States'
+        WHEN TRIM(REPLACE(REPLACE(cntry, CHAR(32), ''), CHAR(13), '')) IN ('UnitedKingdom') THEN 'United Kingdom'
+        WHEN TRIM(REPLACE(REPLACE(cntry, CHAR(32), ''), CHAR(13), '')) = '' OR cntry IS NULL THEN 'NA'
+        ELSE TRIM(REPLACE(REPLACE(cntry, CHAR(32), ''), CHAR(13), '')) 
+	  END AS cntry -- first replacing carriage return with '', then space with '', finally trimming
+FROM datawarehouse.bronze_erp_loc_a101 ;
 
