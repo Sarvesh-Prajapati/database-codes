1cc05317a9ac040c2b4b5f0493ef9f03a96eb5fd
Sarvesh Prajapati <47186476+Sarvesh-Prajapati@users.noreply.github.com>
2025-07-25 10:30:21 +0530

    Update CreateDatesInARange.sql

--- MySQL/Gym/CreateDatesInARange.sql
+++ MySQL/Gym/CreateDatesInARange.sql
@@ -1,4 +1,4 @@
--- WITH RECURSIVE Date_Ranges AS 
+-- WITH RECURSIVE Date_Ranges AS
 -- (
 --     SELECT '2018-11-30' AS Date
 --    UNION ALL
@@ -8,7 +8,7 @@
 
 WITH RECURSIVE Date_Ranges AS 
 (
-    SELECT CURRENT_DATE() AS myDate
+   SELECT CURRENT_DATE() AS myDate
    UNION ALL
    SELECT myDate + INTERVAL 1 DAY FROM Date_Ranges WHERE myDate < '2024-12-31'
 )
